61d7c4ecf2a080e4b7496582b68300950d6f784a
Dalton Pierce <dalton@up2code.nyc>
2020-01-14 11:22:47 -0500

    Added canvas test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
+import App from './App';
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders canvas', () => {
+  const { container } = render(<App />);
+  const canvasElement = container.querySelector('canvas');
+  expect(canvasElement).toBeInTheDocument();
 });
